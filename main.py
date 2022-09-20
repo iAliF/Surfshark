d439058569dd76d55c6ed3c81ff78122427565d2
iAliF <mr.ali.fotouhi@gmail.com>
2022-09-20 14:47:20 +0430

    Generate configs

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import json
 import os.path
 import sys
-from typing import List
+from typing import List, Optional
 
+import requests
 from PySide6.QtWidgets import QApplication, QMessageBox
 from qt_material import apply_stylesheet
+from requests import RequestException
 
 from ui import MainWidget
 
@@ -49,8 +51,41 @@ class Main(MainWidget):
         current = self.locations[self.combo_box.currentIndex()][1]
         self.generate_config(current)
 
-    def generate_config(self, host: str):
-        pass
+    def generate_config(self, host: str) -> None:
+        ip = self.get_ip(host)
+        if ip is None:
+            return
+
+        config = self.default_config
+        config = config.replace('<<<REMOTE>>>', ip).replace('<<<TYPE>>>', 'udp' if self.udp_btn.isChecked() else 'tcp')
+
+        file_name = os.path.join(
+            self.CONFIGS_DIR,
+            f'{self.combo_box.currentText()}.ovpn'
+        )
+
+        with open(file_name, 'w') as file:
+            file.write(config)
+
+        self.show_message_box(
+            QMessageBox.NoIcon,
+            'Done',
+            'Config Successfully generated and saved into configs directory',
+            False
+        )
+
+    def get_ip(self, host) -> Optional[str]:
+        try:
+            req = requests.get(f'http://ip-api.com/line/{host}?fields=query', timeout=5)
+            return req.text.strip()
+        except RequestException:
+            self.show_message_box(
+                QMessageBox.Critical,
+                'Error',
+                'Cannot send request',
+                False
+            )
+            return None
 
     def check_file_exists(self, file: str) -> None:
         if not (os.path.exists(file) and os.path.isfile(file)):
